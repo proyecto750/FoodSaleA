bf783706001c007bc8fa3e8ca0d8d83d0808c8ec
Betzabe Vania Mamani Soliz <vaniamamanisoliz23@gmail.com>
2018-12-13 07:23:22 -0400

    utilidades

--- app/src/main/java/com/example/veronica/foodsale/MainActivity.java
+++ app/src/main/java/com/example/veronica/foodsale/MainActivity.java
@@ -15,79 +15,115 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener , View.OnClickListener{
-private GoogleApiClient api;
-private  final int RC_SIGN_IN = 9001;
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleApiClient api;
+    private  static final int RC_SIGN_IN = 9001;
+
+    private SignInButton signInButton;
+    private Button menu;
+    private Button registro_client;
+    private Button registro_rest;
+    private Button pedidos_rest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        RegisterService();
-        registerEvents();
-    }
-
-    private void registerEvents() {
-        Button btn = (Button)this.findViewById(R.id.google);
-        btn.setOnClickListener((View.OnClickListener) this);
-    }
-
-    private void RegisterService() {
-
         GoogleSignInOptions options = new GoogleSignInOptions
                 .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
-               // .requestProfile()
-                .requestId()
+                // .requestProfile()
                 .build();
         api = new GoogleApiClient.Builder(this)
                 .enableAutoManage(this,this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API,options)
                 .build();
+        signInButton =(SignInButton) findViewById(R.id.signInButton);
+        signInButton.setColorScheme(SignInButton.COLOR_DARK);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =Auth.GoogleSignInApi.getSignInIntent(api);
+                startActivityForResult(intent,RC_SIGN_IN);
+            }
+        });
+        menu =(Button) findViewById(R.id.menu);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CrearMenu.class);
+                startActivity(intent);
+            }
+        });
+
+        registro_client =(Button) findViewById(R.id.registro_client);
+        registro_client.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RegistroUsuario.class);
+                startActivity(intent);
+            }
+        });
+
+        registro_rest = (Button) findViewById(R.id.registro_rest);
+        registro_rest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RegisterRestaurant.class);
+                startActivity(intent);
+            }
+        });
+
+        pedidos_rest =(Button) findViewById(R.id.pedidos_rest);
+        pedidos_rest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,HacerPedidos.class);
+                startActivity(intent);
+            }
+        });
+
 
 
     }
+
+
+
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Toast.makeText(this, "error", Toast.LENGTH_SHORT).show();
 
     }
 
     @Override
-    public void onClick(View view) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode ==RC_SIGN_IN){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
 
-        if (view.getId() == R.id.google){
-            Intent google = Auth.GoogleSignInApi.getSignInIntent(api);
-            this.startActivityForResult(google,RC_SIGN_IN);
+            handleSingInResult(result);
         }
-
     }
 
-    @Override
-    protected void onActivityResult (int r1 , int r2 , Intent data) {
-    super.onActivityResult(r1,r2,data);
-    if (r1 == RC_SIGN_IN){
-        GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-        loadData(result);
-    }
-    }
-
-    private void loadData(GoogleSignInResult result) {
-        if(result.isSuccess()){
-            GoogleSignInAccount datos = result.getSignInAccount();
-            TextView txt = (TextView)this.findViewById(R.id.correo);
-            txt.setText(datos.getEmail());
-            //ImageView Img = (ImageView)this.findViewById(R.id.imageView2);
-            //Img.setImageBitmap(datos.getPhotoUrl());
-
-
-
+    private void handleSingInResult(GoogleSignInResult result) {
+        if (result.isSuccess()){
+            goMainScreen();
 
         }else {
-            Toast.makeText(this,"error no conoces el pasword", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,R.string.not_log_in,Toast.LENGTH_SHORT).show();
         }
+
     }
+    private void goMainScreen(){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK );
+        startActivity(intent);
+    }
+
+
+
 }
---
+++ app/src/main/java/com/example/veronica/foodsale/RegistroUsuario.java
@@ -0,0 +1,13 @@
+package com.example.veronica.foodsale;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistroUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_usuario);
+    }
+}
